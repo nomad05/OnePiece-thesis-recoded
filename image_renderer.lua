6a5b0519b816acddd6608604740e1d16f1e4b010
nomad05 <nomadtest05@gmail.com>
2016-09-13 19:36:04 +0800

    Add files via upload

--- image_renderer.lua
+++ image_renderer.lua
@@ -55,8 +55,42 @@ local options =
 		height = attribute[2],
 		numFrames = attribute[3],
 	}
-print(options["width"])
-print(options)
+local sequenceData =
+	{
+		name=sqAttribute,
+		start=3,
+		count=6,
+		time=100,
+		loopCount = 0,   -- Optional ; default is 0 (loop indefinitely)
+		loopDirection = "bounce"    -- Optional ; values include "forward" or "bounce"
+	}	
+graphics.newImageSheet( location, options )
+display.newSprite( [parent,] imageSheet, sequenceData )
+
+	gifImage = sprite.newSpriteSheet("backgrounds/campaign/portal5.png",127,28)
+				spritel = sprite.newSpriteSet(portal, 1, 6)
+				sprite.add(spritel, "portall", 1, 6, 600, 0)
+
+	--left portal
+			
+			  --portal gif
+				a02gif1 = sprite.newSprite(spritel)
+				a02gif1.x = 100
+				a02gif1.y = display.contentHeight/2 + 70
+				a02gif1:prepare("portall")
+				a02gif1:play()
+				a02gif1:setReferencePoint(display.CenterRightReferencePoint)
+				
+
+				a02portal1= display.newCircle( 100, display.contentHeight/2 + 50, 5 )
+				a02portal1:setReferencePoint(display.CenterRightReferencePoint)
+				a02portal1.alpha = 0
+	bgPath = "backgrounds/"..loc.."/"..stageLvl.."/"..bG..".png"
+	local background =display.newImage(bgPath)
+	background.height = oAHeight
+	background.width = oAWidth
+	background.x = oAWidth/2
+	background.y = oAHeight/2
 	
 end
 return renderer
